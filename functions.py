397637a2c32b60a2aa18e44c70baed13e0ff1ac4
edwin <edwinjoganga@gmail.com>
2023-11-22 15:47:44 +0300

    updates files

---
+++ functions.py
@@ -0,0 +1,33 @@
+# def display_message():
+#     print('Functions are name block of codes that are designed to do one specific job')
+
+# display_message()
+
+
+#favorite book
+# def favorite_book(title):
+#     print('One of my favorites book is ' + title.title())
+
+# favorite_book('art of war')
+
+# 
+# def make_shirt(size = 'large', message = 'I love python'):
+#     print('\nThe shirt is size ' + size.title() + ' with the message "' + message.title() + '" printed on it.')
+
+# # Making a large shirt with the default message
+# make_shirt()
+
+# # Making a medium shirt with the default message
+# make_shirt(size="medium")
+
+# # Making a shirt of any size with a different message
+# make_shirt(size="small", message="Keep Coding!")
+
+def city_country(city, country):
+    print(city.title() + ', ' + country.title())
+
+# Calling the function with city-country pairs
+city_country("santiago", "chile")
+city_country("new york", "united states")
+city_country("tokyo", "japan")
+
